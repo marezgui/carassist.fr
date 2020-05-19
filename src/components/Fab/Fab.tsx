58ab57ec01cd83d0515fe56c1c8ffc78bbcd7660
marezgui <m.rezgui@outlook.fr>
2020-05-20 01:52:54 +0200

    new fab color

--- src/components/Fab/Fab.tsx
+++ src/components/Fab/Fab.tsx
@@ -10,13 +10,19 @@ const Fab = () => {
     "justify-content-start",
   ];
 
+  const buttonClasses = [
+    'ml-md-4', 'mb-md-4', 'ml-3', 'mb-3',
+    'd-flex', 'justify-content-center', 'align-items-center', 
+    'shadow'
+  ];
+
   return (
-    <div className={classes.join(" ")}>
+    <div className={classes.join(' ')}>
       <Button
         as="a"
         href="tel:+33766485866"
-        className="ml-md-4 mb-md-4 ml-3 mb-3 d-flex justify-content-center align-items-center"
-        style={{ height: "50px", width: "50px", borderRadius: "50%" }}
+        className={buttonClasses.join(' ')}
+        style={{ height: "50px", width: "50px", borderRadius: "50%", backgroundImage: 'linear-gradient(72deg,#0033cc 0%,#005bff 100%)', border: '2px solid white' }}
       >
         <CallIcon />
       </Button>
