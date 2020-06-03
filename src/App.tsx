2ec65422314f4790b8167ccaf51576c126a15c6f
marezgui <m.rezgui@outlook.fr>
2020-06-03 18:51:57 +0200

    /garage route removed - Redirect fallback added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Home from './routes/Home/Home';
 import Rent from "./routes/Rent/Rent";
 import Tow from "./routes/Tow/Tow";
-import Garage from "./routes/Garage/Garage";
 
 export default function BasicExample() {
   return (
@@ -22,9 +21,7 @@ export default function BasicExample() {
             <Tow />
           </Route>
 
-          <Route path="/garage">
-            <Garage />
-          </Route>
+          <Redirect to='/' />
         </Switch>
       </>
     </Router>
