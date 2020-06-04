e213133f758605d01f77f6e108a4531e24383b00
marezgui <m.rezgui@outlook.fr>
2020-06-04 13:08:22 +0200

    image slider added

--- src/routes/Rent/data.tsx
+++ src/routes/Rent/data.tsx
@@ -5,36 +5,106 @@ export const cars = {
              title: "Porsche Cayman Martni Racing",
              description:
                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-             src:
-               "https://i.ibb.co/SxfzYx0/Whats-App-Image-2020-05-20-at-22-38-48.jpg",
+             images: [
+               {
+                 alt: "Porsche Cayman au coté d'un hélicoptère.",
+                 src:
+                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/89487905_595120124552711_7040510335215730688_o.jpg?_nc_cat=102&_nc_sid=2d5d41&_nc_ohc=3fPari20Vv8AX-tRlA4&_nc_ht=scontent.fcdg1-1.fna&oh=36e829fa11bbd7ca7cbe4d61dc03a886&oe=5EFF4811",
+               },
+               {
+                 alt: "Porsche Cayman face avant.",
+                 src:
+                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/83877251_578314389566618_3649861679132442624_o.jpg?_nc_cat=103&_nc_sid=2d5d41&_nc_ohc=1xTCaoF79_cAX9OuPJ5&_nc_ht=scontent.fcdg1-1.fna&oh=17e369a1f5e5573cdea061e480deb60b&oe=5EFD1B3D",
+               },
+               {
+                 alt: "Porsche Cayman vue de loin.",
+                 src:
+                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/84562939_578314122899978_808255709157261312_o.jpg?_nc_cat=100&_nc_sid=2d5d41&_nc_ohc=Zsf9G7X4mZUAX-3CXDi&_nc_ht=scontent.fcdg1-1.fna&oh=1200480b4d932b5dc209c90f7e43a9f1&oe=5EFD3DF5",
+               },
+               {
+                 alt: "Porsche Cayman face arrière.",
+                 src:
+                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/85091301_578314836233240_6440439189908488192_o.jpg?_nc_cat=103&_nc_sid=2d5d41&_nc_ohc=2tBsv0VIQ_AAX84fAIe&_nc_ht=scontent.fcdg1-1.fna&oh=145d99f3b381e09628aed37ff5230023&oe=5EFFE76E",
+               },
+             ],
            },
            {
              id: "c2",
              title: "Ford Mustang Us Cabriolet",
              description:
                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-             src:
-               "https://i.ibb.co/646y1PL/Whats-App-Image-2020-05-20-at-22-38-26.jpg",
+             images: [
+               {
+                 alt: "Mustang Cabriolet face avant.",
+                 src:
+                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/p960x960/81749503_550324275698963_3071674337091125248_o.jpg?_nc_cat=107&_nc_sid=9e2e56&_nc_ohc=A6XtjweBvQ8AX-iqK99&_nc_ht=scontent.fcdg1-1.fna&_nc_tp=6&oh=b2ec8166f9b7d846455477bd9697fb6c&oe=5EFF1A2F",
+               },
+               {
+                 alt: "Mustang Cabriolet face arrière.",
+                 src:
+                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/p960x960/81774540_550324455698945_8848626227494256640_o.jpg?_nc_cat=100&_nc_sid=9e2e56&_nc_ohc=_fzh_nipYTQAX_9sBG0&_nc_ht=scontent.fcdg1-1.fna&_nc_tp=6&oh=9fddfe8891c079fd682283f1d9139c75&oe=5EFE6FE3",
+               },
+               {
+                 alt: "Mustang Cabriolet décapoté.",
+                 src:
+                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/p960x960/81479636_550324345698956_2056312770890039296_o.jpg?_nc_cat=109&_nc_sid=9e2e56&_nc_ohc=Ey_kSIgQKc4AX_iEu8K&_nc_ht=scontent.fcdg1-1.fna&_nc_tp=6&oh=bdd8fe3adb20c67cbe6977db660c9497&oe=5EFF11E7",
+               },
+               {
+                 alt: "Mustang Cabriolet interieur.",
+                 src:
+                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/p960x960/80874691_550324565698934_6773001655848796160_o.jpg?_nc_cat=106&_nc_sid=9e2e56&_nc_ohc=KM-UVXFx6twAX_Rzth_&_nc_ht=scontent.fcdg1-1.fna&_nc_tp=6&oh=48ee3dd8244017f439a94b4e2b8fbae1&oe=5EFBB5BD",
+               },
+             ],
            },
            {
              id: "c3",
              title: "Range Rover Sport HSE",
-             src:
-               "https://i.ibb.co/4MgsgHK/Whats-App-Image-2020-05-21-at-17-16-03.jpg",
+             images: [
+               {
+                 alt: "Range Rover avant et coté gauche.",
+                 src:
+                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/81856131_555433878521336_4040363838011342848_o.jpg?_nc_cat=111&_nc_sid=9e2e56&_nc_ohc=eWqehzf3YZ0AX_iTaKa&_nc_ht=scontent.fcdg1-1.fna&oh=be9ff92f350c9e61107a79f2a76fe645&oe=5EFCC3E6",
+               },
+               {
+                 alt: "Range Rover arrière et coté gauche.",
+                 src:
+                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/81674805_555433931854664_656034994375884800_o.jpg?_nc_cat=101&_nc_sid=9e2e56&_nc_ohc=bMsQ6RO9OIYAX_PV-cg&_nc_ht=scontent.fcdg1-1.fna&oh=091ca75285a7b1dee7e20eea00a81037&oe=5F000AB7",
+               },
+               {
+                 alt: "Range Rover interieur.",
+                 src:
+                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/81089051_555434098521314_2777025282639396864_o.jpg?_nc_cat=106&_nc_sid=9e2e56&_nc_ohc=4HVJCRZfm0kAX-qI4nB&_nc_ht=scontent.fcdg1-1.fna&oh=7e3cc6e2e0a0fdf5ea326f64dc332408&oe=5F001298",
+               },
+               {
+                 alt: "Range Rover au coté d'un jet-privé.",
+                 src:
+                   "https://i.ibb.co/4MgsgHK/Whats-App-Image-2020-05-21-at-17-16-03.jpg",
+               },
+             ],
            },
          ],
          truck: [
            {
              id: "t1",
              title: "Camion",
-             src:
-               "https://i.ibb.co/VSDW1Qk/Whats-App-Image-2020-05-21-at-17-23-31.jpg",
+             images: [
+               {
+                 alt: "Camion",
+                 src:
+                   "https://i.ibb.co/2tP79sp/Whats-App-Image-2020-05-21-at-17-23-31.jpg",
+               },
+             ],
            },
            {
              id: "t2",
              title: "Depanneuse",
-             src:
-               "https://i.ibb.co/nsWySxV/Whats-App-Image-2020-05-21-at-17-23-46.jpg",
+             images: [
+               {
+                 alt: "Depanneuse",
+                 src:
+                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/61599198_414502299281162_5209886413419446272_o.jpg?_nc_cat=101&_nc_sid=dd9801&_nc_ohc=5y8tQwJyD0wAX_roTEf&_nc_ht=scontent.fcdg1-1.fna&oh=420e06fd4b633e4aac6195c757d6bb4d&oe=5EFCEAB2",
+               },
+             ],
            },
          ],
        };
