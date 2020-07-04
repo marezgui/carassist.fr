8e68be72fd43604f700520fc045611e42eb73807
marezgui <m.rezgui@outlook.fr>
2020-07-04 22:16:25 +0200

    photos hosted in firebase

--- src/routes/Rent/data.tsx
+++ src/routes/Rent/data.tsx
@@ -2,29 +2,29 @@ export const cars = {
          car: [
            {
              id: "c1",
-             title: "Porsche Cayman Martni Racing",
+             title: "Porsche Cayman Martini Racing",
              description:
                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
              images: [
                {
                  alt: "Porsche Cayman au coté d'un hélicoptère.",
                  src:
-                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/89487905_595120124552711_7040510335215730688_o.jpg?_nc_cat=102&_nc_sid=2d5d41&_nc_ohc=3fPari20Vv8AX-tRlA4&_nc_ht=scontent.fcdg1-1.fna&oh=36e829fa11bbd7ca7cbe4d61dc03a886&oe=5EFF4811",
+                   "https://firebasestorage.googleapis.com/v0/b/car-assist-278209.appspot.com/o/cars%2Fporche%2Fporche_face_helicopter.jpg?alt=media&token=130fc287-7bce-4a9e-8da7-eb8211b1c14f",
                },
                {
                  alt: "Porsche Cayman face avant.",
                  src:
-                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/83877251_578314389566618_3649861679132442624_o.jpg?_nc_cat=103&_nc_sid=2d5d41&_nc_ohc=1xTCaoF79_cAX9OuPJ5&_nc_ht=scontent.fcdg1-1.fna&oh=17e369a1f5e5573cdea061e480deb60b&oe=5EFD1B3D",
+                   "https://firebasestorage.googleapis.com/v0/b/car-assist-278209.appspot.com/o/cars%2Fporche%2Fporche_face-left.jpg?alt=media&token=5cb511cf-59d1-46b9-88be-18d2c5168b7f",
                },
                {
-                 alt: "Porsche Cayman vue de loin.",
+                 alt: "Porsche Cayman face arrière droite.",
                  src:
-                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/84562939_578314122899978_808255709157261312_o.jpg?_nc_cat=100&_nc_sid=2d5d41&_nc_ohc=Zsf9G7X4mZUAX-3CXDi&_nc_ht=scontent.fcdg1-1.fna&oh=1200480b4d932b5dc209c90f7e43a9f1&oe=5EFD3DF5",
+                   "https://firebasestorage.googleapis.com/v0/b/car-assist-278209.appspot.com/o/cars%2Fporche%2Fporche_back-right.jpg?alt=media&token=4f9d599f-49ed-487e-938e-88d3e89a7881",
                },
                {
-                 alt: "Porsche Cayman face arrière.",
+                 alt: "Porsche Cayman face arrière gauche.",
                  src:
-                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/85091301_578314836233240_6440439189908488192_o.jpg?_nc_cat=103&_nc_sid=2d5d41&_nc_ohc=2tBsv0VIQ_AAX84fAIe&_nc_ht=scontent.fcdg1-1.fna&oh=145d99f3b381e09628aed37ff5230023&oe=5EFFE76E",
+                   "https://firebasestorage.googleapis.com/v0/b/car-assist-278209.appspot.com/o/cars%2Fporche%2Fporche_back-left.jpg?alt=media&token=07397477-4a34-46e0-8901-49d5c3907141",
                },
              ],
            },
@@ -35,24 +35,24 @@ export const cars = {
                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
              images: [
                {
-                 alt: "Mustang Cabriolet face avant.",
+                 alt: "Mustang Cabriolet face avant décapoté.",
                  src:
-                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/p960x960/81749503_550324275698963_3071674337091125248_o.jpg?_nc_cat=107&_nc_sid=9e2e56&_nc_ohc=A6XtjweBvQ8AX-iqK99&_nc_ht=scontent.fcdg1-1.fna&_nc_tp=6&oh=b2ec8166f9b7d846455477bd9697fb6c&oe=5EFF1A2F",
+                   "https://firebasestorage.googleapis.com/v0/b/car-assist-278209.appspot.com/o/cars%2Fmustang%2Fmustang_face-opened.jpg?alt=media&token=35e27045-3507-4f59-a51f-6ae84b820211",
                },
                {
-                 alt: "Mustang Cabriolet face arrière.",
+                 alt: "Mustang Cabriolet face arrière droite.",
                  src:
-                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/p960x960/81774540_550324455698945_8848626227494256640_o.jpg?_nc_cat=100&_nc_sid=9e2e56&_nc_ohc=_fzh_nipYTQAX_9sBG0&_nc_ht=scontent.fcdg1-1.fna&_nc_tp=6&oh=9fddfe8891c079fd682283f1d9139c75&oe=5EFE6FE3",
+                   "https://firebasestorage.googleapis.com/v0/b/car-assist-278209.appspot.com/o/cars%2Fmustang%2Fmustang_back-right.jpg?alt=media&token=074a14dc-00bd-4cfd-a6f8-e7c1cec4947e",
                },
                {
-                 alt: "Mustang Cabriolet décapoté.",
+                 alt: "Mustang Cabriolet face arrière droite décapoté.",
                  src:
-                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/p960x960/81479636_550324345698956_2056312770890039296_o.jpg?_nc_cat=109&_nc_sid=9e2e56&_nc_ohc=Ey_kSIgQKc4AX_iEu8K&_nc_ht=scontent.fcdg1-1.fna&_nc_tp=6&oh=bdd8fe3adb20c67cbe6977db660c9497&oe=5EFF11E7",
+                   "https://firebasestorage.googleapis.com/v0/b/car-assist-278209.appspot.com/o/cars%2Fmustang%2Fmustang_back-right-opened.jpg?alt=media&token=dfbadb5f-7405-433f-9099-18050ac8aae6",
                },
                {
                  alt: "Mustang Cabriolet interieur.",
                  src:
-                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/p960x960/80874691_550324565698934_6773001655848796160_o.jpg?_nc_cat=106&_nc_sid=9e2e56&_nc_ohc=KM-UVXFx6twAX_Rzth_&_nc_ht=scontent.fcdg1-1.fna&_nc_tp=6&oh=48ee3dd8244017f439a94b4e2b8fbae1&oe=5EFBB5BD",
+                   "https://firebasestorage.googleapis.com/v0/b/car-assist-278209.appspot.com/o/cars%2Fmustang%2Fmustang_inside.jpg?alt=media&token=53e13316-149d-4e59-8693-c9b830e3d912",
                },
              ],
            },
@@ -63,22 +63,22 @@ export const cars = {
                {
                  alt: "Range Rover avant et coté gauche.",
                  src:
-                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/81856131_555433878521336_4040363838011342848_o.jpg?_nc_cat=111&_nc_sid=9e2e56&_nc_ohc=eWqehzf3YZ0AX_iTaKa&_nc_ht=scontent.fcdg1-1.fna&oh=be9ff92f350c9e61107a79f2a76fe645&oe=5EFCC3E6",
+                   "https://firebasestorage.googleapis.com/v0/b/car-assist-278209.appspot.com/o/cars%2Frangerover%2Frangerover_front-left.jpg?alt=media&token=8d318c37-6c9b-47fc-a92c-54793ea2d36b",
                },
                {
                  alt: "Range Rover arrière et coté gauche.",
                  src:
-                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/81674805_555433931854664_656034994375884800_o.jpg?_nc_cat=101&_nc_sid=9e2e56&_nc_ohc=bMsQ6RO9OIYAX_PV-cg&_nc_ht=scontent.fcdg1-1.fna&oh=091ca75285a7b1dee7e20eea00a81037&oe=5F000AB7",
+                   "https://firebasestorage.googleapis.com/v0/b/car-assist-278209.appspot.com/o/cars%2Frangerover%2Frangerover_back-left.jpg?alt=media&token=643a68db-1686-4283-b33a-f7673ca4986d",
                },
                {
                  alt: "Range Rover interieur.",
                  src:
-                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/81089051_555434098521314_2777025282639396864_o.jpg?_nc_cat=106&_nc_sid=9e2e56&_nc_ohc=4HVJCRZfm0kAX-qI4nB&_nc_ht=scontent.fcdg1-1.fna&oh=7e3cc6e2e0a0fdf5ea326f64dc332408&oe=5F001298",
+                   "https://firebasestorage.googleapis.com/v0/b/car-assist-278209.appspot.com/o/cars%2Frangerover%2Frangerover_inside.jpg?alt=media&token=0cc7e60e-210a-4539-9983-87e9e13ffd93",
                },
                {
-                 alt: "Range Rover au coté d'un jet-privé.",
+                 alt: "Range Rover avant droit.",
                  src:
-                   "https://i.ibb.co/4MgsgHK/Whats-App-Image-2020-05-21-at-17-16-03.jpg",
+                   "https://firebasestorage.googleapis.com/v0/b/car-assist-278209.appspot.com/o/cars%2Frangerover%2Frangerover_front-right.jpg?alt=media&token=f1505e90-f3eb-4122-86b0-6cb2f951cf29",
                },
              ],
            },
@@ -102,7 +102,7 @@ export const cars = {
                {
                  alt: "Depanneuse",
                  src:
-                   "https://scontent.fcdg1-1.fna.fbcdn.net/v/t1.0-9/61599198_414502299281162_5209886413419446272_o.jpg?_nc_cat=101&_nc_sid=dd9801&_nc_ohc=5y8tQwJyD0wAX_roTEf&_nc_ht=scontent.fcdg1-1.fna&oh=420e06fd4b633e4aac6195c757d6bb4d&oe=5EFCEAB2",
+                   "https://firebasestorage.googleapis.com/v0/b/car-assist-278209.appspot.com/o/cars%2Ftruck%2Ftruck.jpg?alt=media&token=1c0babe3-8e40-447a-a903-24ccead34f95",
                },
              ],
            },
